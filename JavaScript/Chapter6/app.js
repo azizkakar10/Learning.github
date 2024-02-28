64336a3fe93cc21a9ca34a84611baae69e92b916
azizkakar10 <azizkakar10@gmail.com>
2024-02-28 16:28:14 +0500

    chapter 5 6 created

---
+++ JavaScript/Chapter6/app.js
@@ -0,0 +1,59 @@
+var x = 25
+var y = 7
+console.log(x % y)
+
+var a = 15
+var b = 4
+console.log((a + 10) * (b - 2))
+
+var c = 12
+var d = 3
+console.log(c * d)
+
+var  e = 10
+var f = 3
+console.log(e += f * 2)
+
+var g = 20
+var h = 4
+console.log((g + 5) / h)
+
+var i = 8
+var j = 3
+console.log((i - 2) * j)
+
+var k = 18
+var l = 5
+console.log(k - l + 2)
+
+var m = 7
+var n = 2
+console.log(m ** n)
+
+var o = 13
+var p = 4
+console.log(o % (p + 2))
+
+var q = 16
+var r = 3
+console.log((q / 2) * r)
+
+var s = 25
+var t = 3
+console.log((s - 1) / t)
+
+var u = 11
+var v = 3
+console.log(u - (v + 2))
+
+var w = 22
+var x = 5
+console.log(w + (x **2))
+
+var y = 19
+var z = 4
+console.log(y % (z + 1))
+
+var a = 10
+var b = 3
+console.log(a * (b - 2))
